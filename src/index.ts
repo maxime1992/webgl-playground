9f7a117c0ad1c498e3f8914203548a73f4701b4f
Maxime Robert <maxime.robert1992@gmail.com>
2020-10-30 16:32:38 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+declare const module;
+
+// avoid hot reload with Parcel
+// https://github.com/parcel-bundler/parcel/issues/289#issuecomment-393106708
+if (module.hot) {
+  module.hot.dispose(() => {
+    window.location.reload();
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  //
+});
