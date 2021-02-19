d26d24fe9d290b14bf65af4cb652cc84b5d6bc12
Maxime Robert <maxime.robert1992@gmail.com>
2021-02-19 18:04:47 +0100

    WIP to apply some other filter

--- src/game/filter.frag.ts
+++ src/game/filter.frag.ts
@@ -6,12 +6,26 @@ precision highp int;
 // constant during a single render pass.
 uniform sampler2D tex;
 
-varying vec2 texCoor;
+uniform vec2 screenSize;
 
 void main() {
-  vec4 textureRgba = texture2D(tex, texCoor);
-  float grayScale = (textureRgba.r + textureRgba.g + textureRgba.b) / 3.0;
-  gl_FragColor = vec4(vec3(grayScale), textureRgba.a);
-  // gl_FragColor = vec4(texCoor, 1, 1.0);
+  mat3 filter = mat3(
+    vec3(-1, 0, 1),
+    vec3(-2, 0, 2),
+    vec3(-1, 0, 1)
+  );
+
+  vec3 finalValue = vec3(0.0);
+
+  for (int r = 0; r < 2; ++r) {
+    for (int c = 0; c < 2; ++c) {
+      float filterVal = filter[r][c];
+      vec2 texCoor = (gl_FragCoord.xy + vec2(r - 1, c - 1)) / vec2(1920,1080);
+      vec3 textureRgba = texture2D(tex, texCoor).rgb;
+      finalValue += abs(textureRgba * filterVal);
+    }
+  }
+
+  gl_FragColor = vec4(finalValue, 1.0);
 }
 `;
