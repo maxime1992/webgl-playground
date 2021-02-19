8934d5fd7b4cfdb708153586605fc99d1d57c7c5
Maxime Robert <maxime.robert1992@gmail.com>
2021-02-19 18:04:12 +0100

    render a black and white image on the second pipeline

--- src/game/filter.frag.ts
+++ src/game/filter.frag.ts
@@ -10,7 +10,8 @@ varying vec2 texCoor;
 
 void main() {
   vec4 textureRgba = texture2D(tex, texCoor);
-  gl_FragColor = textureRgba;
+  float grayScale = (textureRgba.r + textureRgba.g + textureRgba.b) / 3.0;
+  gl_FragColor = vec4(vec3(grayScale), textureRgba.a);
   // gl_FragColor = vec4(texCoor, 1, 1.0);
 }
 `;
